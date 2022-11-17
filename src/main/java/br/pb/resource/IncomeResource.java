14a71726d4ef0a387231bcc084beb693b615ef07
Pedro <evanete@MacBook-Pro-Pedro.local>
2022-11-17 16:44:59 -0300

    feat: income - resource and service

--- src/main/java/br/pb/resource/IncomeResource.java
+++ src/main/java/br/pb/resource/IncomeResource.java
@@ -29,4 +29,31 @@ public class IncomeResource {
     public Response findAll() {
         return Response.ok().entity(new ReturnMsg("Income listed", true, service.findAll())).build();
     }
+
+    @GET
+    @Path("/paged")
+    public Response findAllPaged() {
+        return Response.ok().entity(new ReturnMsg("Income listed", true, service.findAllPaged())).build();
+    }
+
+    @GET
+    @Path("/{idIncome}")
+    public Response findOne(@PathParam("idIncome") Long idIncome) {
+        return Response.ok().entity(new ReturnMsg("Income listed", true, service.findOne(idIncome))).build();
+    }
+
+
+    @PUT
+    @Path("/{idIncome}")
+    public Response update(@PathParam("idIncome") long idIncome, Income income) {
+        service.update(idIncome, income);
+        return Response.ok().entity(new ReturnMsg("Income edited.", true)).build();
+    }
+
+    @DELETE
+    @Path("/{idIncome}")
+    public Response delete(@PathParam("idIncome") Long idIncome) {
+        service.delete(idIncome);
+        return Response.ok().entity(new ReturnMsg("Income deleted.", true)).build();
+    }
 }
--- src/main/java/br/pb/service/IncomeService.java
+++ src/main/java/br/pb/service/IncomeService.java
@@ -3,6 +3,8 @@ package br.pb.service;
 import br.pb.exception.APIException;
 import br.pb.mapper.IncomeMapper;
 import br.pb.model.Income;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -14,6 +16,23 @@ public class IncomeService {
     @Inject
     IncomeMapper incomeMapper;
 
+    public List<Income> findAll() {
+        try {
+            return incomeMapper.findAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new APIException("Error searching incomes", e.getMessage(), Response.Status.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public Income findOne(Long idIncome) {
+
+        return incomeMapper.findOne(idIncome)
+                .orElseThrow(() ->
+                        new APIException("Nenhum app encontrado", Response.Status.NO_CONTENT));
+
+    }
+
     public void save(Income income) {
         try {
             incomeMapper.save(income);
@@ -23,13 +42,27 @@ public class IncomeService {
         }
     }
 
-    public List<Income> findAll() {
-        try {
-            return incomeMapper.findAll();
 
+    public void update(long idIncome, Income income) {
+        try {
+            incomeMapper.update(income);
         } catch (Exception e) {
             e.printStackTrace();
-            throw new APIException("Error searching incomes", e.getMessage(), Response.Status.INTERNAL_SERVER_ERROR);
+            throw new APIException("Error trying to edit income.", e.getMessage(), Response.Status.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    public void delete(Long idIncome) {
+        try {
+            incomeMapper.delete(idIncome);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new APIException("Error trying to delete income.", e.getMessage(), Response.Status.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public Page<Income> findAllPaged() {
+
+        return incomeMapper.findAllPage(PageRequest.of(1,10));
     }
 }
